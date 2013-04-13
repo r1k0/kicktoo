3d857eeda76b4ee872dfcc67bf601983842d0414
r1k0 <erick@openchill.org>
2013-04-13 19:39:04 +0000

    commenting for now back to old grub2 configure

--- modules/bootloader_x86.sh
+++ modules/bootloader_x86.sh
@@ -51,73 +51,75 @@ configure_bootloader_grub() {
     fi
 }
 
-configure_bootloader_grub2() {
-    debug configure_bootloader_grub2 "configuring and deploying grub2"
-
-    for device in "${!grub2_install[@]}"; do
-#       FIXME - only accepts a single option currently (--modules=)        
-        local key=$(echo ${grub2_install["${device}"]} | cut -d'=' -f1)
-        local value=$(echo ${grub2_install["${device}"]} | cut -d'=' -f2)
-    
-        debug configure_bootloader_grub2 "deploying grub2-install $key=$value /dev/${device}"
-        spawn_chroot "grub2-install $key=$value /dev/${device}" || die "Could not deploy grub2-install $key=$value /dev/${device}"
-        #spawn "grub2-install $key=$value /dev/${device}" || die "Could not deploy grub2-install $key=$value /dev/${device}"
-                
-        #spawn_chroot "grub2-install --modules=\"part_gpt mdraid1x lvm xfs\" /dev/sda" || die "Could not deploy with grub2-install on /dev/sda"
-        #spawn_chroot "grub2-install --modules=\"part_gpt mdraid1x lvm xfs\" /dev/sdb" || die "Could not deploy with grub2-install on /dev/sdb"
-    done
-    
-    if [ -n "${bootloader_kernel_args}" ]; then
-        local args=$(echo ${bootloader_kernel_args} | \
-        sed -e 's:{{root_keydev_uuid}}:$(get_uuid ${luks_remdev}):' | \
-        sed -e 's:{{root_key}}:${luks_key}:')
-        debug configure_bootloader_grub2 "GRUB_CMDLINE_LINUX=$(echo ${args}) to /etc/default/grub"
-        spawn "sed -i 's:GRUB_CMDLINE_LINUX=\"\":GRUB_CMDLINE_LINUX=\"'\"${args}\"'\":' ${chroot_dir}/etc/default/grub" || \
-        die "Could not adjust GRUB_CMDLINE_LINUX with bootloader args $(echo ${args})"
-    fi
-    debug configure_grub2 "generating /boot/grub2/grub.cfg"
-    spawn_chroot "grub2-mkconfig -o /boot/grub2/grub.cfg" || die "Could not generate /boot/grub2/grub.cfg"
-}
-
+# good stuff but it breaks for a few cases on default profiles 
+# keeping here for later
 #configure_bootloader_grub2() {
-#    debug configure_bootloader_grub2 "configuring /boot/grub/grub.cfg"
-#    echo -e "set default=0\nset timeout=5\n" > ${chroot_dir}/boot/grub/grub.cfg
-#    local boot_root="$(get_boot_and_root)"
-#    local boot="$(echo ${boot_root} | cut -d '|' -f1)"
-#    local boot_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
-#    local boot_minor="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f2)"
-#    local root="$(echo ${boot_root} | cut -d '|' -f2)"
-#    local kernel_initrd="$(get_kernel_and_initrd)"
+#    debug configure_bootloader_grub2 "configuring and deploying grub2"
 #
-#    for k in ${kernel_initrd}; do
-#        local kernel="$(echo ${k}  | cut -d '|' -f1)"
-#        local initrd="$(echo ${k}  | cut -d '|' -f2)"
-#        local kv="$(echo ${kernel} | sed -e 's:^kernel-*-[^-]\+-::' | sed -e 's:[^-]\+-::')"
-#        echo "menuentry \"${distro} Linux ${kv}\" {" >> ${chroot_dir}/boot/grub/grub.cfg
-#        local grub_device="$(map_device_to_grub2_device ${boot_device})"
-#        if [ -z "${grub_device}" ]; then
-#            error "Could not map boot device ${boot_device} to grub device"
-#            return 1
-#        fi
-#        echo -en "set root=(${grub_device},$(expr ${boot_minor}))\nlinux /${kernel} " >> ${chroot_dir}/boot/grub/grub.cfg
-#        if [ -z "${initrd}" ]; then
-#            echo "root=${root}" >> ${chroot_dir}/boot/grub/grub.cfg
-#        else
-#            echo "root=/dev/ram0 init=/linuxrc ramdisk=8192 real_root=${root} ${bootloader_kernel_args}" >> ${chroot_dir}/boot/grub/grub.cfg
-#            echo -e "initrd /${initrd}\n" >> ${chroot_dir}/boot/grub/grub.cfg
-#        fi
-#        echo -e "}\n" >> ${chroot_dir}/boot/grub/grub.cfg
+#    for device in "${!grub2_install[@]}"; do
+##       FIXME - only accepts a single option currently (--modules=)        
+#        local key=$(echo ${grub2_install["${device}"]} | cut -d'=' -f1)
+#        local value=$(echo ${grub2_install["${device}"]} | cut -d'=' -f2)
+#    
+#        debug configure_bootloader_grub2 "deploying grub2-install $key=$value /dev/${device}"
+#        spawn_chroot "grub2-install $key=$value /dev/${device}" || die "Could not deploy grub2-install $key=$value /dev/${device}"
+#        #spawn "grub2-install $key=$value /dev/${device}" || die "Could not deploy grub2-install $key=$value /dev/${device}"
+#                
+#        #spawn_chroot "grub2-install --modules=\"part_gpt mdraid1x lvm xfs\" /dev/sda" || die "Could not deploy with grub2-install on /dev/sda"
+#        #spawn_chroot "grub2-install --modules=\"part_gpt mdraid1x lvm xfs\" /dev/sdb" || die "Could not deploy with grub2-install on /dev/sdb"
 #    done
-#    if ! spawn_chroot "grep -v rootfs /proc/mounts > /etc/mtab"; then
-#        error "Could not copy /proc/mounts to /etc/mtab"
-#        return 1
+#    
+#    if [ -n "${bootloader_kernel_args}" ]; then
+#        local args=$(echo ${bootloader_kernel_args} | \
+#        sed -e 's:{{root_keydev_uuid}}:$(get_uuid ${luks_remdev}):' | \
+#        sed -e 's:{{root_key}}:${luks_key}:')
+#        debug configure_bootloader_grub2 "GRUB_CMDLINE_LINUX=$(echo ${args}) to /etc/default/grub"
+#        spawn "sed -i 's:GRUB_CMDLINE_LINUX=\"\":GRUB_CMDLINE_LINUX=\"'\"${args}\"'\":' ${chroot_dir}/etc/default/grub" || \
+#        die "Could not adjust GRUB_CMDLINE_LINUX with bootloader args $(echo ${args})"
 #    fi
-#    [ -z "${bootloader_install_device}" ] && bootloader_install_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
-#    if ! spawn_chroot "grub-install ${bootloader_install_device}"; then
-#        error "Could not install grub to ${bootloader_install_device}"
-#        return 1
-#    fi
+#    debug configure_grub2 "generating /boot/grub2/grub.cfg"
+#    spawn_chroot "grub2-mkconfig -o /boot/grub2/grub.cfg" || die "Could not generate /boot/grub2/grub.cfg"
 #}
+
+configure_bootloader_grub2() {
+    debug configure_bootloader_grub2 "configuring /boot/grub/grub.cfg"
+    echo -e "set default=0\nset timeout=5\n" > ${chroot_dir}/boot/grub/grub.cfg
+    local boot_root="$(get_boot_and_root)"
+    local boot="$(echo ${boot_root} | cut -d '|' -f1)"
+    local boot_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
+    local boot_minor="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f2)"
+    local root="$(echo ${boot_root} | cut -d '|' -f2)"
+    local kernel_initrd="$(get_kernel_and_initrd)"
+
+    for k in ${kernel_initrd}; do
+        local kernel="$(echo ${k}  | cut -d '|' -f1)"
+        local initrd="$(echo ${k}  | cut -d '|' -f2)"
+        local kv="$(echo ${kernel} | sed -e 's:^kernel-*-[^-]\+-::' | sed -e 's:[^-]\+-::')"
+        echo "menuentry \"${distro} Linux ${kv}\" {" >> ${chroot_dir}/boot/grub/grub.cfg
+        local grub_device="$(map_device_to_grub2_device ${boot_device})"
+        if [ -z "${grub_device}" ]; then
+            error "Could not map boot device ${boot_device} to grub device"
+            return 1
+        fi
+        echo -en "set root=(${grub_device},$(expr ${boot_minor}))\nlinux /${kernel} " >> ${chroot_dir}/boot/grub/grub.cfg
+        if [ -z "${initrd}" ]; then
+            echo "root=${root}" >> ${chroot_dir}/boot/grub/grub.cfg
+        else
+            echo "root=/dev/ram0 init=/linuxrc ramdisk=8192 real_root=${root} ${bootloader_kernel_args}" >> ${chroot_dir}/boot/grub/grub.cfg
+            echo -e "initrd /${initrd}\n" >> ${chroot_dir}/boot/grub/grub.cfg
+        fi
+        echo -e "}\n" >> ${chroot_dir}/boot/grub/grub.cfg
+    done
+    if ! spawn_chroot "grep -v rootfs /proc/mounts > /etc/mtab"; then
+        error "Could not copy /proc/mounts to /etc/mtab"
+        return 1
+    fi
+    [ -z "${bootloader_install_device}" ] && bootloader_install_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
+    if ! spawn_chroot "grub-install ${bootloader_install_device}"; then
+        error "Could not install grub to ${bootloader_install_device}"
+        return 1
+    fi
+}
 
 configure_bootloader_lilo() {
     debug configure_bootloader_lilo "configuring /etc/lilo.conf"
